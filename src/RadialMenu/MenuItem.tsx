ad80222558275d313a25aeac0709046ac2806436
Adam Kowalski <adam@adam.local>
2022-11-11 20:24:41 -0800

    revamped radial menu

---
+++ src/RadialMenu/MenuItem.tsx
@@ -0,0 +1,52 @@
+import { createSignal } from "solid-js"
+import { styled } from "solid-styled-components"
+import { Option } from "../menu"
+
+export const diameter = 44
+const distance = 50
+const delta = Math.PI / 3
+
+const Circle = styled("div")({
+    position: "absolute",
+    color: "#ffffff",
+    "-webkit-backdrop-filter": "blur(10px)",
+    "backdrop-filter": "blur(10px)",
+    "border-radius": "50%",
+    width: `${diameter}px`,
+    height: `${diameter}px`,
+    display: "flex",
+    "justify-content": "center",
+    "align-items": "center",
+    "font-size": "20px",
+    cursor: "pointer",
+})
+
+interface Props {
+    i: () => number
+    option: Option
+    hide: () => void
+}
+
+export const MenuItem = (props: Props) => {
+    const theta = delta * props.i()
+    const x = Math.cos(theta) * distance
+    const y = Math.sin(theta) * distance
+    const translate = `translate(${x}px, ${-y}px)`
+    const [hover, setHover] = createSignal(false)
+    return (
+        <Circle
+            style={{
+                transform: translate,
+                background: hover() ? "#000000AA" : "#00000066",
+            }}
+            onPointerEnter={() => setHover(true)}
+            onPointerLeave={() => setHover(false)}
+            onPointerUp={() => {
+                props.option.onClick()
+                props.hide()
+            }}
+        >
+            <props.option.icon />
+        </Circle>
+    )
+}
