2abb8d558245959efa6e02816d05a53514b922c0
Adam Kowalski <adam@adam.local>
2022-11-25 22:14:25 -0800

    move body content test to its own file

--- test/body_content.test.tsx
+++ test/body_content.test.tsx
@@ -1,60 +1,14 @@
 import { render, fireEvent } from "@solidjs/testing-library"
-import { uuid } from "fast-check"
 import { JSXElement } from "solid-js"
 import { vi } from "vitest"
 
-import { Body, GraphProvider } from "../src/Graph"
+import { GraphProvider } from "../src/Graph"
 import { createGraph, UUID } from "../src/Graph/graph"
 import { BodyContent } from "../src/Graph/NodeCards/BodyContent"
-import { Positions, PositionsContext } from "../src/Graph/positions"
-import { MeasureText, MeasureTextContext } from "../src/MeasureText"
+import { PositionsContext } from "../src/Graph/positions"
+import { MeasureTextContext } from "../src/MeasureText"
 
-import { Value, base, call } from "../src/value"
 import { Vec2 } from "../src/vec2"
-
-test("add 2 and 3 to get 5", () => {
-    const a: Value = {
-        type: "Number",
-        data: 3,
-    }
-    const b: Value = {
-        type: "Number",
-        data: 2,
-    }
-    const c: Value = call(base, "add", [a, b])
-    expect(c).toEqual({
-        type: "Tensor",
-        data: 5,
-        size: 1,
-        shape: [],
-        rank: 0,
-        dtype: "float32",
-    })
-})
-
-test("add 2 and [1, 2, 3] to get [3, 4, 5]", () => {
-    const a: Value = {
-        type: "Number",
-        data: 2,
-    }
-    const b: Value = {
-        type: "Tensor",
-        data: [1, 2, 3],
-        size: 3,
-        shape: [3],
-        rank: 1,
-        dtype: "float32",
-    }
-    const c: Value = call(base, "add", [a, b])
-    expect(c).toEqual({
-        type: "Tensor",
-        data: [3, 4, 5],
-        size: 3,
-        shape: [3],
-        rank: 1,
-        dtype: "float32",
-    })
-})
 
 export const MockMeasureTextProvider = (props: { children: JSXElement }) => {
     const width = vi.fn<[string, string], number>(() => 0)
